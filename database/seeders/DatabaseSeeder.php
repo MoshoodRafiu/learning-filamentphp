bb06ba787c7d2ec889d866bab3c2b6a0ca22f28a
Rafiu Moshood <meezyloaded@gmail.com>
2023-11-14 18:56:13 +0100

    chore: add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory()->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Moshood',
+            'email' => 'moshood@test.com',
+        ]);
     }
 }
